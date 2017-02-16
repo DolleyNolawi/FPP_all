bda1e7f21e815a85bd1c8b54fd7a1f8c6b4f0935
DolleyNolawi <mer.world@gmail.com>
2017-02-15 22:51:09 -0500

    Deadlock in threads

---
+++ src/org/meron/collection/InsertionList.java
@@ -0,0 +1,23 @@
+package org.meron.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InsertionList {
+
+	public static void main(String[] args) {
+		
+		//list maintains the insertion order if we don't
+		//specify the position to insert it will add it at the last 
+		List<Integer> list = new ArrayList<>();
+		list.add(2);
+		list.add(3);
+		list.add(5);
+		list.add(4);
+		list.set(0, 7);
+		for(int l : list){
+			System.out.print(l);
+		}
+	}
+
+}
