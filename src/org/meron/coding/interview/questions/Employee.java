e75791c297350e40ebd52bea9fd5c39730769577
DolleyNolawi <mer.world@gmail.com>
2017-02-19 21:15:58 -0500

    comparator based on age and name

--- src/org/meron/coding/interview/questions/Employee.java
+++ src/org/meron/coding/interview/questions/Employee.java
@@ -1,10 +1,18 @@
 package org.meron.coding.interview.questions;
 
-public class Employee {
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Employee{
 	
 	private String fname;
 	private int age;
 	private String department;
+	
+	public Employee(){}
+	
 	public Employee(String fname, int age, String department) {
 		super();
 		this.fname = fname;
@@ -28,8 +36,43 @@ public class Employee {
 	}
 	public void setDepartment(String department) {
 		this.department = department;
+	}	
+	
+	@Override
+	public String toString() {
+		return "Employee [fname=" + fname + ", age=" + age + ", department=" + department + "]";
 	}
-	
-	
-
+		
+	public static void main(String args[]){
+		
+		List<Employee> list = Arrays.asList(new Employee("Zeron" , 30, "CS"),
+				new Employee("Dolley" , 20, "DB"),
+				new Employee("Nolawi" , 3, "Medical"),
+				new Employee("Abebe", 30, "CS")
+				);			
+				
+	    Collections.sort(list , new nameAlphaComparator());
+	    Collections.sort( list , new AgeComparator());
+	    
+		for(Employee e : list){
+			System.out.println(e.toString());
+		}	
+		
+	}
 }
+
+class AgeComparator implements Comparator<Employee> {
+    @Override
+    public int compare(Employee a, Employee b) {
+        return a.getAge()< b.getAge() ? -1 : a.getAge() == b.getAge() ? 0 : 1;
+    }
+}
+
+class nameAlphaComparator implements Comparator<Employee>{
+
+	@Override
+	public int compare(Employee o1, Employee o2) {
+		return o1.getFname().compareToIgnoreCase(o2.getFname());
+	}	
+}
+
